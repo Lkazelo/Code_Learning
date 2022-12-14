167afcfbe3614499c14b213cecfeee84bc90efb1
Lkazelo <89434749+Lkazelo@users.noreply.github.com>
2022-12-14 10:58:58 +0800

    Add files via upload

---
+++ 动态数组/动态数组/动态数组/01动态数组.c
@@ -0,0 +1,31 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+#include"DynamicArray.h"
+
+
+void test01() {
+	Dynamic_Array* myArray = Init_Array();
+
+	//打印容量
+	printf("数组容量:%d\n",Capacity_Array(myArray));
+	printf("数组容量:%d\n", Size_Array(myArray));
+	//插入元素
+	for (int i =0;i<30;i++) {
+		PushBack_Array(myArray,i);
+	}
+	//打印
+	Print_Array(myArray);
+	printf("数组容量:%d\n", Capacity_Array(myArray));
+	printf("数组容量:%d\n", Size_Array(myArray));
+	//销毁
+	FreeSpace_Array(myArray);
+}
+
+int main(void) {
+
+	test01();
+	system("pause");
+	return 0;
+}
---
+++ 动态数组/动态数组/动态数组/DynamicArray.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+typedef struct DYNAMICARRAY{
+	int* pAddr; //存放数据的地址
+	int size; //存放数组中元素个数
+	int capacity;//容量
+}Dynamic_Array;
+
+Dynamic_Array* Init_Array();
+//插入
+void PushBack_Array(Dynamic_Array* arr,int value);
+//删除
+void RemoveByPos_Array(Dynamic_Array* arr,int pos);
+void RemoveByValue_Array(Dynamic_Array* arr, int value);
+//查找
+int Find_Array(Dynamic_Array* arr,int value);
+//打印
+void Print_Array(Dynamic_Array* arr);
+//释放动态数组的内存
+void FreeSpace_Array(Dynamic_Array* arr);
+
+//清空数组
+void Clear_Array(Dynamic_Array* arr);
+//获得动态数组容量
+int Capacity_Array(Dynamic_Array* arr);
+//获得动态数组当前元素个数
+int Size_Array(Dynamic_Array* arr);
+//根据位置获得某个位置元素
+int At_Array(Dynamic_Array* arr,int pos);
+
